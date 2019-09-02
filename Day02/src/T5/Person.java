a358813e00693630fae6418fbf8ecb0443bbdeed
sanjiaomaojl <1026845550@qq.com>
2019-09-02 19:02:28 +0800

    interface's demo

---
+++ Day02/src/T5/Person.java
@@ -0,0 +1,46 @@
+package T5;
+
+public abstract class Person {
+    private String name;
+    private String gender;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, String gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public void sleep(){
+        System.out.println("睡觉(¦3[▓▓] 晚安)");
+    }
+
+    public abstract void eat();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ Day02/src/T5/Smoke.java
@@ -0,0 +1,5 @@
+package T5;
+
+public interface Smoke {
+    void smoker();
+}
