710148600e6ca4d4244b142b2c902e2955543673
sanjiaomaojl <1026845550@qq.com>
2019-09-03 20:38:16 +0800

    HomeWork6

---
+++ Day03/src/T6/TSix.java
@@ -0,0 +1,10 @@
+package T6;
+
+import java.util.Calendar;
+
+public class TSix {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        System.out.println(c.get(Calendar.DAY_OF_YEAR));
+    }
+}
