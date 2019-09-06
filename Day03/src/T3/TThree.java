4f518eb947b95020dca65e7773be5a04bcd8d4d7
sanjiaomaojl <1026845550@qq.com>
2019-09-06 21:14:56 +0800

    T three test

---
+++ Day03/src/T3/TThree.java
@@ -0,0 +1,19 @@
+package T3;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class TThree {
+    public static void main(String[] args){
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, 2018);
+        c.set(Calendar.MARCH, 1);
+        c.set(Calendar.DATE, 14);
+
+        int dayOfWeek = Calendar.DAY_OF_WEEK;
+
+
+    }
+}
