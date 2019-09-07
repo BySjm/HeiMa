3cb72b006b5401edd3682054dffde9a6bd262bc3
sanjiaomaojl <1026845550@qq.com>
2019-09-07 14:23:29 +0800

    Test 2、3、4

---
+++ Day05/src/T2/T2.java
@@ -0,0 +1,36 @@
+package T2;
+
+import java.util.ArrayList;
+
+public class T2 {
+
+    public static void main(String[] args) {
+        ArrayList<String> list1 = new ArrayList<>();
+        list1.add("张");
+        System.out.println(listTest(list1, "张"));
+        System.out.println(listTest(list1));
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(4);
+        list2.add(3);
+        list2.add(6);
+        System.out.println("第一次出现2的索引" + listTest(list2, 2));
+    }
+
+    public static boolean listTest(ArrayList<String> al, String s){
+        return al.contains(s);
+    }
+
+    public static boolean listTest(ArrayList<String> al){
+        return al.isEmpty();
+    }
+
+    public static int listTest(ArrayList<Integer> al, Integer s){
+        for (int i = 0; i < al.size(); i++) {
+            if (al.get(i) == s)
+                return i;
+        }
+        return -1;
+    }
+
+}
