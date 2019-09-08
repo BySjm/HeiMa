3ae35d9f3a7de467d34ee803c12edce3a90da088
sanjiaomaojl <1026845550@qq.com>
2019-09-08 10:40:42 +0800

    Test8

---
+++ Day05/src/T8/TestDemo08.java
@@ -0,0 +1,30 @@
+package T8;
+
+import java.util.ArrayList;
+
+public class TestDemo08 {
+    public static void main(String[] args) {
+        ArrayList<People> list = new ArrayList<>();
+        list.add(new People("张三", 180));
+        list.add(new People("张四", 175));
+        list.add(new People("张五", 170));
+        list.add(new People("张六", 165));
+        list.add(new People("李四", 160));
+        find(list);
+    }
+
+    public static void find(ArrayList<People> list) {
+        int highest = 0;
+        int shortest = 0;
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i).getHeight() > list.get(highest).getHeight())
+                highest = i;
+            else
+                shortest = i;
+        }
+        People high = list.get(highest);
+        People low = list.get(shortest);
+        System.out.println("最高的人是" + high.getName() + ",身高" + high.getHeight());
+        System.out.println("最矮的人是" + low.getName() + ",身高" + low.getHeight());
+    }
+}
