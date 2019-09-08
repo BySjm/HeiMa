ca6b8f4d3fc2a6f33a823e8dc2104a130055c529
sanjiaomaojl <1026845550@qq.com>
2019-09-08 16:41:44 +0800

    Test15

---
+++ Day05/src/T15/Test.java
@@ -0,0 +1,14 @@
+package T15;
+
+import java.util.Collection;
+
+public class Test {
+
+    public <E> void addElement(Collection<E> c, E data){
+        c.add(data);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
