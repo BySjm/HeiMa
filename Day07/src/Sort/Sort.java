9f8e73de7b513a4ba4ba581af5604097869a3642
sanjiaomaojl <1026845550@qq.com>
2019-09-09 19:46:14 +0800

    Sort demo

---
+++ Day07/src/Sort/Sort.java
@@ -0,0 +1,37 @@
+package Sort;
+
+public class Sort {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, 3, 2, 6};
+        SelectSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void SelectSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void MaoPao(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+}
