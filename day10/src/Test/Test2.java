d1e5d3ec9435d77560bd0d83b1116d19d37e6a0c
sanjiaomaojl <1026845550@qq.com>
2019-09-15 19:31:17 +0800

    TestDemoCode

--- day10/src/Test/Test2.java
+++ day10/src/Test/Test2.java
@@ -5,7 +5,7 @@ import java.io.FileOutputStream;
 
 public class Test2 {
     public static void main(String[] args) throws Exception {
-        FileInputStream fis = new FileInputStream("abc.txt");
+        FileInputStream fis = new FileInputStream("测试文件.txt");
         FileOutputStream fos = new FileOutputStream("测试文件.txt");
         int temp = 0;
         while ((temp = fis.read()) != -1) {
