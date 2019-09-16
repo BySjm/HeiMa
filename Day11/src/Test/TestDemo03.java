2976594dd182cadd3a15f61ccc30f30c633ab6ad
sanjiaomaojl <1026845550@qq.com>
2019-09-16 18:15:28 +0800

    TestDemo

---
+++ Day11/src/Test/TestDemo03.java
@@ -0,0 +1,15 @@
+package Test;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+public class TestDemo03 {
+    public static void main(String[] args) throws Exception {
+        BufferedInputStream bufin = new BufferedInputStream(new FileInputStream("123.txt"));
+        int len = 0;
+        while ((len = bufin.read()) != -1) {
+            System.out.print((char) len);
+        }
+        bufin.close();
+    }
+}
