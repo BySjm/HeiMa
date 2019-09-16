18bdb12ba7ada2a87b971bb2ab0cd26b107b86f6
sanjiaomaojl <1026845550@qq.com>
2019-09-16 20:15:15 +0800

    T6

--- Day11/src/HomeWork/T6/T6.java
+++ Day11/src/HomeWork/T6/T6.java
@@ -1,6 +1,8 @@
 package HomeWork.T6;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class T6 {
@@ -9,5 +11,12 @@ public class T6 {
         OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream("a.txt"), "GBK");
         osw.write(content);
         osw.close();
+        InputStreamReader isr = new InputStreamReader(new FileInputStream("a.txt"), "GBK");
+        char[] chars = new char[10];
+        int len = -1;
+        while ((len = isr.read(chars)) != -1){
+            System.out.println(new String(chars, 0, len));
+        }
+        isr.close();
     }
 }
