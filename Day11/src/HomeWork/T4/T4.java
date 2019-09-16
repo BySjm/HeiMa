bb31f06900e081dea1c66da1306219ca94933ccb
sanjiaomaojl <1026845550@qq.com>
2019-09-16 18:15:12 +0800

    T4

---
+++ Day11/src/HomeWork/T4/T4.java
@@ -0,0 +1,19 @@
+package HomeWork.T4;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class T4 {
+    public static void main(String[] args) throws Exception{
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(""));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(""));
+        int b = 0;
+        while ((b = bis.read()) != -1){
+            bos.write(b);
+        }
+        bos.close();
+        bis.close();
+    }
+}
