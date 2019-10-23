c29e391c853d56b08d12cac66083d71d17590331
sanjiaomaojl <1026845550@qq.com>
2019-10-23 22:43:08 +0800

    mybatis 一级缓存技术测试

--- Day30_nest/src/cn/bysjm/test/UserMapperText.java
+++ Day30_nest/src/cn/bysjm/test/UserMapperText.java
@@ -2,6 +2,8 @@ package cn.bysjm.test;
 
 import cn.bysjm.domain.User;
 import cn.bysjm.mapper.UserMapper;
+import cn.bysjm.util.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 import java.util.List;
@@ -26,4 +28,15 @@ public class UserMapperText extends BaseMapperTest{
         }
     }
 
+    @Test
+    public void testOneCache() throws Exception {
+        SqlSession sqlSession = MybatisUtils.openSession();
+        UserMapper userMapper1 = sqlSession.getMapper(UserMapper.class);
+        User user1 = userMapper1.findById(41);
+        System.err.println(user1);
+//        sqlSession.clearCache();
+        UserMapper userMapper2 = sqlSession.getMapper(UserMapper.class);
+        User user2 = userMapper2.findById(41);
+        System.err.println(user2);
+    }
 }
